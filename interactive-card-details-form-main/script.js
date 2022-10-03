47432597860a1fcdf3a42cb4dfaeda0a731ddb63
Hugo Amoglia Priori <priori.hugo@estudante.ufjf.br>
2022-10-03 12:38:04 -0300

    implementing validation script

--- interactive-card-details-form-main/script.js
+++ interactive-card-details-form-main/script.js
@@ -1,36 +1,66 @@
-const cardNameInput = document.getElementById("cardholder-name");
-const cardNameDisplay = document.getElementsByClassName("cardholder-name-display");
-
-let cardNumberInput = document.getElementById("card-number");
-let cardNumberDisplay = document.getElementsByClassName("card-number-display");
-
-const cardExpireMonthInput = document.getElementById("exp-date-month");
-const cardExpireYearInput = document.getElementById("exp-date-year");
-const cardExpireDisplay = document.getElementsByClassName("exp-date-display");
-
-const cardCVCInput = document.getElementById("cvc");
-const cardCVCDisplay = document.getElementsByClassName("cvc-display");
-
 const form = document.getElementById("form");
-
 const NUM_INPUT_MAX_SIZE = 16;
 
-cardNumberInput.addEventListener("input", (e) => {
+const inputs = document.querySelectorAll('input');
+inputs.forEach((input)=>{
+  input.addEventListener('input' , (event) =>{
+    
+    const type = event.target.name;
+    const value = event.target.value.trim();
+    const {valid , data} = processInput[type](value);
+    const displayValue = value == "" ? inputDefault[type] : data;
+    const displayDiv = document.getElementsByClassName(type + '-display');
+    console.log(displayDiv);
+    displayDiv[0].innerHTML = displayValue;
 
-  let value = getValue(e);
-  let display = treatNumberFormat(e.target.value);
+    if(valid){
+      console.log('valid')
+    }else{
 
-  const numberRE = /((\s)*(\d{4})(\s)*){4}/;
-  cardNumberDisplay[0].innerHTML = display;
+    }
 
-  if (value.match(numberRE) && value.length == NUM_INPUT_MAX_SIZE) {
-    console.log("match");
-  } else {
-    console.log("doesnt match");
+  })
+})
+const inputDefault = {}
+inputs.forEach((input)=>{
+
+  inputDefault[input.name] = document.getElementsByClassName(input.name + '-display')[0].innerHTML;
+})
+
+//this object is used to treat every entry properly in a single location in code and format the display when needed
+//its very important paying attention to div classes and ids patterns
+const processInput = {
+  'cardholder-name' : (value) =>{
+    return {valid : testRegEx(/^([a-z]{2,})\s([a-z]{2,})$/i , value) , data : value }
+  },
+  'card-number' : (value)=>{
+    return {valid : testRegEx(/^((\s)*(\d{4})(\s)*){4}$/ , value) , data : treatNumberFormat(value)}
+  },
+  'exp-date-month' : (value)=>{
+    const intValue = parseInt(value , 10);
+    const displayValue = value.slice(0 , 2);
+
+    return {valid : testRegEx(/^(\d){2}$/ , value) && inRange(intValue , 1 , 12), data : displayValue}
+  },
+  'exp-date-year' : (value)=>{
+    const intValue = parseInt(value , 10);
+    const displayValue = value.slice(0 , 2);
+
+    return {valid : testRegEx(/^(\d){2}$/ , value) && inRange(intValue , 0 , 99), data : displayValue}
+  },
+  'cvc' : (value)=>{
+    const displayValue = value.slice(0,3);
+    return {valid : testRegEx(/^(\d){3}$/ , value) , data : displayValue}
   }
-});
-function getValue(e) {
-  return e.target.value.trim();
+};
+
+
+function testRegEx(regEx , value){
+  return regEx.test(value);
+}
+function inRange(value , lowLimit , highLimit){
+  console.log(value , (value <= lowLimit && value >=highLimit) );
+  return(value >= lowLimit && value <= highLimit);
 }
 function treatNumberFormat(message) {
   message.replace(/\s/g, "");
@@ -39,16 +69,13 @@ function treatNumberFormat(message) {
   for (let i = 0; i < NUM_INPUT_MAX_SIZE - message.length && i >= 0; i++) {
     treated = treated.concat("x");
   }
-
   treated = treated.slice(0, 16);
-
   let substr = [
     treated.slice(0, 4),
     treated.slice(4, 8),
     treated.slice(8, 12),
     treated.slice(12, 16),
   ];
-
   treated = substr.join(" ");
 
   return treated;
