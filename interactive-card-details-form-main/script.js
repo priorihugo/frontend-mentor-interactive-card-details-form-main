59d5114a39e9b88e46e4c761aa30052127e8231f
Hugo Amoglia Priori <priori.hugo@estudante.ufjf.br>
2022-10-04 12:39:58 -0300

    implementing error message script

--- interactive-card-details-form-main/script.js
+++ interactive-card-details-form-main/script.js
@@ -4,19 +4,34 @@ const NUM_INPUT_MAX_SIZE = 16;
 const inputs = document.querySelectorAll('input');
 inputs.forEach((input)=>{
   input.addEventListener('input' , (event) =>{
+
+    let errs = '';
     
     const type = event.target.name;
     const value = event.target.value.trim();
-    const {valid , data} = processInput[type](value);
-    const displayValue = value == "" ? inputDefault[type] : data;
+    const {valid , data , err} = processInput[type](value);
+
+    let displayValue;
+    if(value == ''){
+      displayValue = inputDefault[type];
+      errs = 'Cant be Blank';
+    }else{
+      displayValue = data;
+      errs = err;
+    }
     const displayDiv = document.getElementsByClassName(type + '-display');
-    console.log(displayDiv);
     displayDiv[0].innerHTML = displayValue;
+
+    const errMsgDisplay = event.target.parentElement.getElementsByClassName('err-msg');
 
     if(valid){
       console.log('valid')
+      event.target.classList.remove('invalid');
+      errMsgDisplay[0].classList.remove('show-message');
     }else{
-
+      event.target.classList.add('invalid');
+      errMsgDisplay[0].classList.add('show-message');
+      errMsgDisplay[0].innerHTML = errs;
     }
 
   })
@@ -31,26 +46,26 @@ inputs.forEach((input)=>{
 //its very important paying attention to div classes and ids patterns
 const processInput = {
   'cardholder-name' : (value) =>{
-    return {valid : testRegEx(/^([a-z]{2,})\s([a-z]{2,})$/i , value) , data : value }
+    return {valid : testRegEx(/^([a-z]{2,})\s([a-z]{2,})$/i , value) , data : value , err : 'Wrong format, letters only , two names required'}
   },
   'card-number' : (value)=>{
-    return {valid : testRegEx(/^((\s)*(\d{4})(\s)*){4}$/ , value) , data : treatNumberFormat(value)}
+    return {valid : testRegEx(/^((\s)*(\d{4})(\s)*){4}$/ , value) , data : treatNumberFormat(value) , err : 'Wrong format, numbers only'}
   },
   'exp-date-month' : (value)=>{
     const intValue = parseInt(value , 10);
     const displayValue = value.slice(0 , 2);
 
-    return {valid : testRegEx(/^(\d){2}$/ , value) && inRange(intValue , 1 , 12), data : displayValue}
+    return {valid : testRegEx(/^(\d){2}$/ , value) && inRange(intValue , 1 , 12), data : displayValue , err : 'Wrong format, numbers only'}
   },
   'exp-date-year' : (value)=>{
     const intValue = parseInt(value , 10);
     const displayValue = value.slice(0 , 2);
 
-    return {valid : testRegEx(/^(\d){2}$/ , value) && inRange(intValue , 0 , 99), data : displayValue}
+    return {valid : testRegEx(/^(\d){2}$/ , value) && inRange(intValue , 0 , 99), data : displayValue , err : 'Wrong format, numbers only'}
   },
   'cvc' : (value)=>{
     const displayValue = value.slice(0,3);
-    return {valid : testRegEx(/^(\d){3}$/ , value) , data : displayValue}
+    return {valid : testRegEx(/^(\d){3}$/ , value) , data : displayValue , err : 'Wrong format, numbers only'}
   }
 };
 
@@ -63,8 +78,10 @@ function inRange(value , lowLimit , highLimit){
   return(value >= lowLimit && value <= highLimit);
 }
 function treatNumberFormat(message) {
-  message.replace(/\s/g, "");
+  message = message.replace(/\s/g, "");
   let treated = message;
+
+  console.log(treated)
 
   for (let i = 0; i < NUM_INPUT_MAX_SIZE - message.length && i >= 0; i++) {
     treated = treated.concat("x");
